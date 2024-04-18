136338a64c90a5eca1e110a8d66c9be4320a14d6
AMEDOMEY Romeo David <davidromeoamedomey@yahoo.com>
2024-04-18 11:05:08 +0200

    add post resource endpoint

--- api/routes.js
+++ api/routes.js
@@ -5,9 +5,11 @@ const router = Router();
 const equipmentController = require('./controllers/equipmentController');
 const occupationTypeController = require('./controllers/occupationTypeController');
 const authorizedUserController = require('./controllers/authorizedUserController');
+const resourceController = require('./controllers/resourceController');
 
 router.get('/equipments', equipmentController.getAllEquipment);
 router.get('/occupation-types', occupationTypeController.getAllTypes);
 router.get('/authorized-users', authorizedUserController.getAllAuthorizedUsers);
+router.post('/resources', resourceController.createResource);
 
 module.exports = router;
