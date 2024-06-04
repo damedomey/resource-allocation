e7dd99a903e164d09a6647622ee54b419c2caed8
AMEDOMEY Romeo David <davidromeoamedomey@yahoo.com>
2024-06-04 07:09:41 +0200

    partial: find available resources

--- api/routes.js
+++ api/routes.js
@@ -3,10 +3,15 @@ const {Router} = require('express');
 const router = Router();
 
 const resourceController = require('./controllers/resourceController');
+const reservationController = require('./controllers/reservationController');
 
 router.post('/resources', resourceController.createResource);
 router.get('/resources/form', resourceController.getForm);
 router.get('/resources', resourceController.getAllResources);
 router.get('/resources/:id', resourceController.getResourceById);
 router.delete('/resources/:id', resourceController.deleteResource);
+
+router.post('/reservations/check-availability', reservationController.getAvailableResources);
+router.post('/reservations', reservationController.makeReservation);
+
 module.exports = router;
