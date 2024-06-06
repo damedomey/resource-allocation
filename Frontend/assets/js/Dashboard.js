676c79f63b6ded67af4db88c1d30c8630523d4ee
ELYSALEM99 <elysalemm5@gmail.com>
2024-06-07 01:33:51 +0200

    dashbord avancement

--- Frontend/assets/js/Dashboard.js
+++ Frontend/assets/js/Dashboard.js
@@ -2,21 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let resources = [];
     let reservations = [];
 
-    // Chargement des données depuis l'API
-    fetch('http://localhost:3000/resources')
-        .then(response => response.json())
-        .then(data => {
-            resources = data; // Stocke les données dans la variable 'resources'
-            updateLayout(resources);
-        });
-
-    // Charger les réservations
-    fetch('http://localhost:3000/reservations')
-        .then(response => response.json())
-        .then(data => {
-            reservations = data;
-            updateLayout();  // Mettre à jour après chargement des deux sets de données
-        });
+    // Chargement des données depuis l'API et mise à jour de l'affichage
+    Promise.all([
+        fetch('http://localhost:3000/resources').then(response => response.json()),
+        fetch('http://localhost:3000/reservations').then(response => response.json())
+    ]).then(data => {
+        resources = data[0];
+        reservations = data[1];
+        updateLayout(resources);  // Mise à jour initiale avec toutes les ressources
+    });
 
     // Initialisation du tooltip pour afficher des détails
     const tooltip = d3.select('body').select('div.container').select('div.row').append('div')
@@ -35,49 +29,74 @@ document.addEventListener("DOMContentLoaded", function () {
     const spacing = 10;
     const maxSvgWidth = 960;
 
-    // Fonction pour mettre à jour la mise en page des ressources
+
     function updateLayout(filteredResources) {
         const svgWidth = Math.min(window.innerWidth, maxSvgWidth);
         const numItemsPerRow = Math.floor(svgWidth / (rectWidth + spacing));
         const svgHeight = (Math.ceil(filteredResources.length / numItemsPerRow) * (rectHeight + spacing)) + spacing;
-
+    
         let svg = d3.select('svg');
         if (svg.empty()) {
             svg = d3.select('body').append('svg');
         }
-
+    
         svg.attr('viewBox', `0 0 ${svgWidth} ${svgHeight}`)
             .attr('preserveAspectRatio', 'xMinYMin meet')
             .style('width', '100%')
             .style('height', 'auto');
-
-        const reservedIds = new Set(reservations.map(r => r.resourceId));  // Créer un set des IDs réservés
-        const rects = svg.selectAll('rect').data(filteredResources, d => d.id);
-        rects.enter().append('rect').merge(rects)
-            .attr('x', (_, i) => (i % numItemsPerRow) * (rectWidth + spacing) + spacing)
-            .attr('y', (_, i) => Math.floor(i / numItemsPerRow) * (rectHeight + spacing) + spacing)
-            .attr('width', rectWidth)
-            .attr('height', rectHeight)
-            //.attr('fill', 'lightblue')
-            .attr('fill', d => reservedIds.has(d.id) ? 'red' : 'green')
-            .on('mouseover', function (event, d) {
-                const infoBox = document.getElementById('infoBox');
-                infoBox.innerHTML = `Nom: ${d.resourceName}<br>Capacité Max: ${d.maxCapacity}<br>Taille: ${d.areaSize}<br>Type: ${d.resourceType}<br>ID: ${d.id}`;
-                infoBox.style.visibility = 'visible';
-            })
-            .on('mousemove', function (event) {
-                const infoBox = document.getElementById('infoBox');
-                infoBox.style.left = (event.pageX + 15) + 'px'; // 15 pixels à droite du curseur
-                infoBox.style.top = (event.pageY + 15) + 'px'; // 15 pixels en dessous du curseur
-            })
-            .on('mouseout', function () {
-                const infoBox = document.getElementById('infoBox');
-                infoBox.style.visibility = 'hidden';
-            });
-
-
-
-        rects.exit().remove();
+    
+        // Clean up existing groups
+        svg.selectAll('g').remove();
+    
+        // Create a group for each resource
+        const groups = svg.selectAll('g')
+                          .data(filteredResources, d => d.id)
+                          .enter()
+                          .append('g')
+                          .attr('transform', (_, i) => `translate(${(i % numItemsPerRow) * (rectWidth + spacing) + spacing}, ${Math.floor(i / numItemsPerRow) * (rectHeight + spacing) + spacing})`);
+    
+        // Add rectangles to each group
+        groups.append('rect')
+              .attr('width', rectWidth)
+              .attr('height', rectHeight)
+              .attr('fill', d => {
+                  const rate = calculateOccupancy(d.id, d.maxCapacity);
+                  return rate > 0 ? 'red' : 'green';
+              });
+    
+        // Add text to each group
+        groups.append('text')
+              .attr('x', rectWidth / 2)
+              .attr('y', rectHeight / 2)
+              .attr('dy', '0.35em')
+              .attr('text-anchor', 'middle')
+              .style('font-size', '12px')
+              .text(d => truncateText(d.resourceName, 10));
+    
+        // Mouseover events for the rectangles
+        groups.select('rect')
+              .on('mouseover', function (event, d) {
+                  const occupancyRate = calculateOccupancy(d.id, d.maxCapacity);
+                  const infoBox = document.getElementById('infoBox');
+                  infoBox.innerHTML = `Nom: ${d.resourceName}<br>Capacité Max: ${d.maxCapacity}<br>Area Size: ${d.areaSize}<br>Type: ${d.resourceType}<br>Taux d'occupation: ${occupancyRate}%`;
+                  infoBox.style.visibility = 'visible';
+              })
+              .on('mousemove', function (event) {
+                  const infoBox = document.getElementById('infoBox');
+                  infoBox.style.left = (event.pageX + 15) + 'px'; // 15 pixels à droite du curseur
+                  infoBox.style.top = (event.pageY + 15) + 'px'; // 15 pixels en dessous du curseur
+              })
+              .on('mouseout', function () {
+                  const infoBox = document.getElementById('infoBox');
+                  infoBox.style.visibility = 'hidden';
+              });
+    
+        // Remove any old groups that are no longer needed
+        svg.selectAll('g').data(filteredResources, d => d.id).exit().remove();
+    }
+    
+    function truncateText(text, maxLength) {
+        return text.length > maxLength ? text.substring(0, maxLength - 3) + '...' : text;
     }
 
     // Filtrer les ressources en fonction des critères sélectionnés
@@ -101,6 +120,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
         updateLayout(filteredResources);
     };
+
+    function calculateOccupancy(resourceId, maxCapacity) {
+        maxCapacity = parseInt(maxCapacity, 10);
+        const reservationsForThisResource = reservations.filter(r => r.resourceId === resourceId);
+        const totalPeople = reservationsForThisResource.reduce((acc, curr) => acc + curr.numberOfPeople, 0);
+        const occupancyRate = (totalPeople / maxCapacity) * 100;
+        return occupancyRate.toFixed(0);
+    }
 
     // Événement pour gérer le redimensionnement de la fenêtre
     window.addEventListener('resize', () => updateLayout(resources));
